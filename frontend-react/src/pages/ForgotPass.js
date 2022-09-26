7d57d3caf40758eef53d69e52d4f676bc82faf2e
Hong Xian <yap.hong.xian@gmail.com>
2022-09-26 23:10:40 +0800

    rebased

--- frontend-react/src/pages/ForgotPass.js
+++ frontend-react/src/pages/ForgotPass.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -56,63 +55,4 @@ class ForgotPass extends Component {
     }
 }
 
-=======
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-class ForgotPass extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            username: '',
-            password: '',
-        }
-        this.changeUsername = this.changeUsername.bind(this);
-        this.changePassword = this.changePassword.bind(this);
-    }
-
-    changeUsername(event) {
-        this.setState({username: event.target.value});
-    }
-
-    changePassword(event) {
-        this.setState({password: event.target.value});
-    }
-
-    changePassClicked = (event) => {
-        event.preventDefault();
-        let username = this.state.username;
-        let password = this.state.password;
-        let authHeader = window.btoa(username + ':' + password);
-        let user = {'username': username, 'authHeader': authHeader};
-        localStorage.setItem('user', JSON.stringify(user));
-        this.props.history.push('/login');
-    }
-
-    render() {
-        return (
-            <div className="container">
-                <br></br>
-                <div className="card col-md-6 offset-md-3 offset-md-3">
-                    <div className = "card-body">
-                        <form>
-                            <div>
-                                <label>Username</label>
-                                <input placeholder="username" name="username" className="form-control" value={this.state.username} onChange={this.changeUsername}/>
-                            </div>
-                            <div>
-                                <label>New password</label>
-                                <input placeholder="Your new password..." name="password" className="form-control" value={this.state.password} onChange={this.changePassword}/>
-                            </div>
-                            <br></br>
-                            <button className="btn btn-success" onClick={this.changePassClicked}>Change password</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-}
-
->>>>>>> 2e6e741818ecf9e299b0b6589de4cba6f5a6c00e
 export default ForgotPass
