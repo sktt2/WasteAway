7cd9ee853445c5f5330a2a48b6dc9581e578d157
francis1009 <francism56789@gmail.com>
2022-10-02 12:20:45 +0800

    Add form validation to add product page and cleaned up code

--- frontend-react/src/pages/AddProduct.js
+++ frontend-react/src/pages/AddProduct.js
@@ -8,18 +8,9 @@ class AddProduct extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            productname: '',
-            category: '',
-            description: '',
-            conditions: '',
-            image: null,
-            user: null
+            user: null,
+            validated: ''
         }
-        this.changeProductname = this.changeProductname.bind(this)
-        this.changeCategory = this.changeCategory.bind(this)
-        this.changeDescription = this.changeDescription.bind(this)
-        this.changeConditions = this.changeConditions.bind(this)
-        this.uploadImage = this.uploadImage.bind(this)
         this.validateInputs = this.validateInputs.bind(this)
         this.addProductClicked = this.addProductClicked.bind(this)
     }
@@ -30,56 +21,30 @@ class AddProduct extends Component {
         this.setState({user: res.data})
     }
 
-    changeProductname(event) {
-        this.setState({productname: event.target.value})
-    }
-
-    changeCategory(event) {
-        this.setState({category: event.target.value})
-    }
-
-    changeDescription(event) {
-        this.setState({description: event.target.value})
-    }
-
-    changeConditions(event) {
-        this.setState({conditions: event.target.value})
-    }
-
-    uploadImage(event) {
-        // save image into database
-        this.setState({image: URL.createObjectURL(event.target.files[0])})
-    }
-
     // Return proper error modal on invalid input
     validateInputs = (event) => {
-        if (this.state.productname == '' || this.state.category == 'DEFAULT' || this.state.description == '' || this.state.conditions == '') {
-            return false;
+        const form = event.currentTarget
+        if (form.checkValidity() === false) {
+            event.preventDefault()
+            event.stopPropagation()
         } else {
             this.addProductClicked(event)
         }
+        this.setState({validated: 'was-validated'})
     }
 
     addProductClicked = (event) => {
         event.preventDefault();
-        let productname = this.state.productname
-        let category = this.state.category
-        if (category == 'DEFAULT') {
-            category = null
-        }
-        let description = this.state.description
-        let conditions = this.state.conditions
-        let image = this.state.image
-        let user = this.state.user
         let newProduct = {
-            productName: productname,
-            category: category,
-            description: description,
-            condition: conditions,
+            productName: this.state.productname,
+            category: this.state.category,
+            description: this.state.description,
+            condition: this.state.conditions,
             dateTime: new Date().toISOString(),
-            imageUrl: image,
-            user: user
+            imageUrl: this.state.image,
+            user: this.state.user
         }
+        console.log(JSON.stringify(newProduct))
         ProductService.addProduct(newProduct)
         .then((response) => {
             console.log(response)
@@ -97,44 +62,67 @@ class AddProduct extends Component {
                 <br></br>
                 <div className="card col-md-6 offset-md-3 offset-md-3">
                     <div className = "card-body">
-                        <form>
+                        <form noValidate class={this.state.validated} onSubmit={this.validateInputs}>
                             <div>
-                                <label>Product Name</label>
-                                <Form.Control placeholder="Product name" name="productname" className="form-control" value={this.state.productname} onChange={this.changeProductname} required/>
+                                <Form.Group>
+                                    <Form.Label>Product Name</Form.Label>
+                                    <Form.Control placeholder="Product name" name="productname" className="form-control" value={this.state.productname} onChange={event => this.setState({productname: event.target.value})} required/>
+                                    <Form.Control.Feedback type="invalid">
+                                        Please provide a product name.
+                                    </Form.Control.Feedback>
+                                </Form.Group>
                             </div>
                             <div>
-                                <label>Category</label>
-                                <Form.Select value={this.state.category} onChange={this.changeCategory}>
-                                    <option value="DEFAULT" hidden>Select category</option>
-                                    <option value="BOOKS">Books</option>
-                                    <option value="ELECTRONICS">Electronics</option>
-                                    <option value="FASHION">Fashion</option>
-                                    <option value="FOOD">Food</option>
-                                    <option value="TOYS">Toys</option>
-                                    <option value="UTILITY">Utility</option>
-                                    <option value="VIDEOGAMES">Video Games</option>
-                                    <option value="OTHERS">Others</option>
-                                </Form.Select>
+                                <Form.Group>
+                                    <Form.Label>Category</Form.Label>
+                                    <Form.Select value={this.state.category} onChange={event => this.setState({category: event.target.value})} required>
+                                        <option value="" hidden>Select category</option>
+                                        <option value="BOOKS">Books</option>
+                                        <option value="ELECTRONICS">Electronics</option>
+                                        <option value="FASHION">Fashion</option>
+                                        <option value="FOOD">Food</option>
+                                        <option value="TOYS">Toys</option>
+                                        <option value="UTILITY">Utility</option>
+                                        <option value="VIDEOGAMES">Video Games</option>
+                                        <option value="OTHERS">Others</option>
+                                    </Form.Select>
+                                    <Form.Control.Feedback type="invalid">
+                                        Select a category.
+                                    </Form.Control.Feedback>
+                                </Form.Group>
                             </div>
                             <div>
-                                <label>Description</label>
-                                <Form.Control placeholder="Description" name="description" as="textarea" rows={3} value={this.state.description} onChange={this.changeDescription} required/>
+                                <Form.Group>
+                                    <Form.Label>Description</Form.Label>
+                                    <Form.Control placeholder="Description" name="description" as="textarea" rows={3} value={this.state.description} onChange={event => this.setState({description: event.target.value})} required/>
+                                    <Form.Control.Feedback type="invalid">
+                                        Please provide the product description.
+                                    </Form.Control.Feedback>
+                                </Form.Group>
                             </div>
                             <div>
-                                <label>Condition of Product</label>
-                                <Form.Control placeholder="Conditions" name="conditions" as="textarea" rows={3} value={this.state.conditions} onChange={this.changeConditions} required/>
+                                <Form.Group>
+                                    <Form.Label>Condition of Product</Form.Label>
+                                    <Form.Control placeholder="Conditions" name="conditions" as="textarea" rows={3} value={this.state.conditions} onChange={event => this.setState({conditions: event.target.value})} required/>
+                                    <Form.Control.Feedback type="invalid">
+                                        Please provide the product condition.
+                                    </Form.Control.Feedback>
+                                </Form.Group>
                             </div>
                             <div>
                                 <Form.Group controlId="formFile" className="mb-3">
                                     <Form.Label>Product Image</Form.Label>
-                                    <Form.Control type="file" accept="image/*" onChange={this.uploadImage}/>
+                                    <Form.Control type="file" accept="image/*" onChange={event => this.setState({image: URL.createObjectURL(event.target.files[0])})} required/>
+                                    <Form.Control.Feedback type="invalid">
+                                        Please provide the product image.
+                                    </Form.Control.Feedback>
                                 </Form.Group>
                                 <div>
                                     <img className="container-fluid w-100" src={this.state.image}/>
                                 </div>
                             </div>
                             <br></br>
-                            <button className="btn btn-success" onClick={this.validateInputs}>Add Product</button>
+                            <button className="btn btn-success" type="submit">Add Product</button>
                         </form>
                     </div>
                 </div>
