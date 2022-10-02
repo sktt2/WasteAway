9febc2e93f647b26930db9f24dcd8a9f357c9b18
timothy huang <krhuang.2021@scis.smu.edu.sg>
2022-10-02 20:25:48 +0800

    + Remove product api + Update product detail api + Get User's product api

--- backend-spring-boot/src/main/java/csd/app/controllers/ProductController.java
+++ backend-spring-boot/src/main/java/csd/app/controllers/ProductController.java
@@ -6,11 +6,14 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import csd.app.payload.response.MessageResponse;
 import csd.app.payload.response.ProductResponse;
 import csd.app.product.Product;
 import csd.app.product.ProductNotFoundException;
 import csd.app.product.ProductRepository;
+import csd.app.user.User;
 
+import org.springframework.web.bind.annotation.GetMapping;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -38,6 +41,41 @@ public class ProductController {
             .orElseThrow(() -> new ProductNotFoundException(id));
         ProductResponse resp = new ProductResponse(id, product.getProductName(), product.getCondition(), product.getDateTime(), product.getDescription(), product.getCategory(), product.getImageUrl(), product.getUser());
         return ResponseEntity.ok(resp);
+    }
+
+    @GetMapping("api/products/user/{id}")
+    public List<ProductResponse> getProductByOwner(User user) {
+        List<Product> products = productRepository.findByUser(user);
+        List<ProductResponse> resp = new ArrayList<>();
+        for (Product product : products) {
+            ProductResponse prodResp = new ProductResponse(product.getId(), product.getProductName(), product.getCondition(), product.getDateTime(), product.getDescription(), product.getCategory(), product.getImageUrl(),product.getUser());
+            resp.add(prodResp);
+        }
+        return resp;
+    }
+    
+    @PutMapping("api/products/update/{id}")
+    public ResponseEntity<?> updateProductDetail(@RequestBody Product PR){
+        Product product = productRepository.findById(PR.getId()).orElseThrow(() -> new ProductNotFoundException(PR.getId()));
+        if (PR.getCategory() != null && PR.getCondition() != null && PR.getDateTime() != null && 
+            PR.getImageUrl() != null && PR.getProductName() != null){
+                product.setCategory(PR.getCategory());
+                product.setCondition(PR.getCondition());
+                product.setDateTime(PR.getDateTime());
+                product.setDescription(PR.getDescription());
+                product.setImageUrl(PR.getImageUrl());
+                product.setProductName(PR.getProductName());
+                productRepository.save(product);
+                return ResponseEntity.ok(new MessageResponse("Product detail updated successfully"));
+            }
+        return ResponseEntity.ok(new MessageResponse("Failed to update product detail"));
+    }
+
+    @DeleteMapping("api/products/remove/{id}")
+    public ResponseEntity<?> removeProduct(@PathVariable Long id){
+        Product product = productRepository.findById(id).orElseThrow(() -> new ProductNotFoundException(id));
+        productRepository.delete(product);
+        return ResponseEntity.ok(new MessageResponse("Product has been removed"));
     }
 
 }
--- backend-spring-boot/src/main/java/csd/app/product/ProductRepository.java
+++ backend-spring-boot/src/main/java/csd/app/product/ProductRepository.java
@@ -1,7 +1,12 @@
 package csd.app.product;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import csd.app.user.User;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByUser(User user);
 }
