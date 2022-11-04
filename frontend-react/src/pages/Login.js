c0a2991e1468c999729230a01a31ee70f9940e70
Dong Xian <dxsim.2021@scis.smu.edu.sg>
2022-11-04 21:51:32 +0800

    refactor login page

--- frontend-react/src/pages/Login.js
+++ frontend-react/src/pages/Login.js
@@ -1,183 +1,164 @@
 import React, { Component } from "react"
-import "bootstrap/dist/css/bootstrap.min.css"
 
-import Form from "react-bootstrap/Form"
-import Button from "react-bootstrap/Button"
-import Alert from "react-bootstrap/Alert"
 import AuthService from "../services/AuthService"
 import VisibilityIcon from "@mui/icons-material/Visibility"
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff"
 import IconButton from "@mui/material/IconButton"
 // Import CSS styling
-import styles from "../styles/ComponentStyle.module.css"
 import StorageHelper from "../services/StorageHelper"
+import { Box } from "@mui/system"
 
+import {
+    FormControl,
+    Grid,
+    InputLabel,
+    OutlinedInput,
+    Button,
+    Alert,
+    InputAdornment,
+} from "@mui/material"
 class Login extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            showpassword: false,
-            isrememberme: false,
-            errormessage: "",
-            messageDisplay: false,
-            validated: "",
+            showPassword: false,
+            errorMessage: "",
+            displayError: false,
+            username: "",
+            password: "",
         }
-        this.validateInputs = this.validateInputs.bind(this)
         this.loginClicked = this.loginClicked.bind(this)
         this.toggleShowPassword = this.toggleShowPassword.bind(this)
+        this.handleChange = this.handleChange.bind(this)
     }
 
     componentDidMount() {
         if (localStorage.hasOwnProperty("user")) {
             this.props.history.push("/")
         }
-        if (localStorage.hasOwnProperty("rememberme")) {
-            var retrieveUsername = JSON.parse(localStorage.getItem("rememberme"))
-            this.setState({ username: retrieveUsername.username })
-        }
     }
 
-    // TODO
-    // Import Formik library for custom form validation
-
-    validateInputs = (event) => {
-        const form = event.currentTarget
-        if (form.checkValidity() === false) {
-            event.preventDefault()
-            event.stopPropagation()
-        } else {
-            this.loginClicked(event)
-        }
-        this.setState({ validated: "was-validated" })
+    handleChange = (field) => {
+        this.setState(field)
     }
-
     loginClicked = (event) => {
         event.preventDefault()
-        AuthService.signin(this.state.username, this.state.password)
+        this.setState({ displayError: false })
+        const { username, password } = this.state
+        if (username.length === 0 || password.length === 0) {
+            this.setState({
+                errorMessage: "Username/Password is blank",
+                displayError: true,
+            })
+            return
+        }
+        AuthService.signin(username, password)
             .then((response) => {
                 StorageHelper.setUser(JSON.stringify(response.data))
-                if (this.state.isrememberme) {
-                    localStorage.setItem("rememberme", JSON.stringify(response.data))
-                } else {
-                    if (localStorage.hasOwnProperty("rememberme")) {
-                        localStorage.removeItem("rememberme")
-                    }
-                }
                 this.props.history.push("/products")
             })
             .catch((err) => {
                 this.setState({
-                    errormessage: "Username/Password is incorrect!",
-                    messageDisplay: true,
+                    errorMessage: "Username/Password is incorrect!",
+                    displayError: true,
                 })
             })
     }
 
     toggleShowPassword = () => {
-        this.setState({ showpassword: !this.state.showpassword })
-    }
-
-    isRememberMeClicked = () => {
-        if (this.state.isrememberme === false) {
-            this.setState({ isrememberme: true })
-        } else {
-            this.setState({ isrememberme: false })
-        }
+        this.setState({ showPassword: !this.state.showPassword })
     }
 
     render() {
         return (
-            <div className="container">
-                <br></br>
-                <div className="card col-md-6 offset-md-3 offset-md-3">
-                    <div className={styles.inputCard}>
-                        <Alert
-                            variant="danger"
-                            show={this.state.messageDisplay}
-                            onClose={() => this.setState({ messageDisplay: false })}
-                            dismissible>
-                            {this.state.errormessage}
-                        </Alert>
-                        <Form
-                            noValidate
-                            className={this.state.validated}
-                            onSubmit={this.validateInputs}>
-                            <div>
-                                <Form.Group>
-                                    <Form.Label>Username</Form.Label>
-                                    <Form.Control
-                                        placeholder="Username"
-                                        name="username"
-                                        required
-                                        value={this.state.username || ""}
-                                        onChange={(event) =>
-                                            this.setState({ username: event.target.value })
-                                        }
-                                    />
-                                    <Form.Control.Feedback type="invalid">
-                                        Enter a username.
-                                    </Form.Control.Feedback>
-                                </Form.Group>
-                            </div>
-                            <div>
-                                <Form.Group>
-                                    <Form.Label htmlFor="password-input">Password</Form.Label>
-                                    <div className="input-group">
-                                        <Form.Control
-                                            type={this.state.showpassword ? "text" : "password"}
-                                            id="password-input"
-                                            required
-                                            value={this.state.password || ""}
-                                            onChange={(event) =>
-                                                this.setState({ password: event.target.value })
-                                            }
-                                            placeholder="Password"
-                                        />
-                                        <div className="input-group-append">
+            <Box display="flex" sx={{ justifyContent: "center", padding: "5vw 0" }}>
+                <Box
+                    component="form"
+                    display="flex"
+                    sx={{
+                        border: "1px solid",
+                        flexDirection: "column",
+                        padding: "1vw 0",
+                        borderRadius: 2,
+                        width: "50%",
+                    }}>
+                    <Grid
+                        container
+                        direction="column"
+                        alignItems="center"
+                        justifyContent="center"
+                        style={{ width: "100%" }}
+                        rowSpacing={1}>
+                        <Grid item>
+                            {this.state.displayError ? (
+                                <Alert severity="error">{this.state.errorMessage}</Alert>
+                            ) : (
+                                <Box />
+                            )}
+                        </Grid>
+                        <Grid item sx={{ width: "70%" }}>
+                            <FormControl sx={{ width: "100%" }}>
+                                <InputLabel htmlFor="component-outlined">Username</InputLabel>
+                                <OutlinedInput
+                                    fullWidth={true}
+                                    id="component-outlined"
+                                    value={this.state.username}
+                                    onChange={(event) =>
+                                        this.handleChange({ username: event.target.value })
+                                    }
+                                    label="Username"
+                                />
+                            </FormControl>
+                        </Grid>
+                        <Grid item sx={{ width: "70%", margin: "1vh" }}>
+                            <FormControl sx={{ width: "100%" }} variant="outlined">
+                                <InputLabel htmlFor="outlined-adornment-password">
+                                    Password
+                                </InputLabel>
+                                <OutlinedInput
+                                    id="outlined-adornment-password"
+                                    type={this.state.showPassword ? "text" : "password"}
+                                    value={this.state.password}
+                                    onChange={(event) =>
+                                        this.handleChange({ password: event.target.value })
+                                    }
+                                    endAdornment={
+                                        <InputAdornment position="end">
                                             <IconButton
-                                                aria-label="showpassword"
-                                                onClick={() => this.toggleShowPassword()}
-                                                sx={{
-                                                    borderRadius: "0px 4px 4px 0px",
-                                                    border: "1px solid rgba(195,202,210)",
-                                                    borderWidth: "1px 1px 1px 0px",
-                                                }}>
-                                                {this.state.showpassword ? (
+                                                aria-label="toggle password visibility"
+                                                onClick={this.toggleShowPassword}
+                                                edge="end">
+                                                {this.state.showPassword ? (
                                                     <VisibilityIcon />
                                                 ) : (
                                                     <VisibilityOffIcon />
                                                 )}
                                             </IconButton>
-                                        </div>
-                                        <Form.Control.Feedback type="invalid">
-                                            Enter a password.
-                                        </Form.Control.Feedback>
-                                    </div>
-                                </Form.Group>
-                            </div>
-                            <div>
-                                <Form.Check
-                                    id="flexCheckDefault"
-                                    onChange={this.isRememberMeClicked}
-                                    label={"Remember Me"}
+                                        </InputAdornment>
+                                    }
+                                    label="Password"
                                 />
-                            </div>
-                            <a href="/forgotpass" className="card-link">
-                                Forgot password?
-                            </a>
-                            <br></br>
-                            <Button className="btn btn-success" type="submit">
-                                Login
-                            </Button>{" "}
+                            </FormControl>
+                        </Grid>
+                        <Grid item sx={{ width: "70%" }}>
                             <Button
-                                className="btn btn-success"
+                                color="success"
+                                type="submit"
+                                sx={{ marginRight: "2vh" }}
+                                variant="contained"
+                                onClick={this.loginClicked}>
+                                Login
+                            </Button>
+                            <Button
+                                variant="contained"
+                                color="success"
                                 onClick={() => this.props.history.push("/register")}>
                                 Register
                             </Button>
-                        </Form>
-                    </div>
-                </div>
-            </div>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Box>
         )
     }
 }
