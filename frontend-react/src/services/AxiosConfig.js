055c7f94ef284bafe17279bf422daac83842b31b
Dong Xian <dxsim.2021@scis.smu.edu.sg>
2022-11-06 00:57:59 +0800

    update url

--- frontend-react/src/services/AxiosConfig.js
+++ frontend-react/src/services/AxiosConfig.js
@@ -1,5 +1,4 @@
-export const API_URL =
-    process.env.REACT_APP_API_URL + ":" + process.env.REACT_APP_API_PORT + "/api/"
+export const API_URL = process.env.REACT_APP_API_URL + ":" + process.env.REACT_APP_API_PORT + "/api"
 export const header = {
     "Access-Control-Allow-Origin": "*",
 }
