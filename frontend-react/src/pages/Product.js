46a1c58830c639cdf5db3a3aac4930fc2a091da5
Dong Xian <dxsim.2021@scis.smu.edu.sg>
2022-11-09 11:18:59 +0800

    design fix

--- frontend-react/src/pages/Product.js
+++ frontend-react/src/pages/Product.js
@@ -9,8 +9,6 @@ import CarouselComponent from "../components/Carousel"
 import ProductService from "../services/ProductService"
 import UserService from "../services/UserService"
 import StorageHelper from "../services/StorageHelper"
-    
-
 
 const responsive = {
     desktop: {
@@ -29,52 +27,53 @@ class Product extends Component {
             mainData: [],
             recommendData: [],
             searchText: "",
-            filter: ""
+            filter: "",
         }
     }
 
     async componentDidMount() {
         const res = await ProductService.getProducts()
 
-        if (StorageHelper.getUser()){
-
+        if (StorageHelper.getUser()) {
             const user = await UserService.getUser(StorageHelper.getUsername())
             //if user has not declared recommendation, redirect to recommend page
             if (user.data.firstTime === true) {
                 this.props.history.push("/recommendation")
             }
             //get recommended items for carousel
-            const recommend = await ProductService.getRecommendation(StorageHelper.getUserId())       
-            res.data.forEach(element => {
-                if(element.category === recommend.data && element.ownerName !== StorageHelper.getUsername()){
+            const recommend = await ProductService.getRecommendation(StorageHelper.getUserId())
+            res.data.forEach((element) => {
+                if (
+                    element.category.toLowerCase() === recommend.data.toLowerCase() &&
+                    element.ownerName !== StorageHelper.getUsername()
+                ) {
                     this.state.recommendData.push(element)
                 }
             })
 
-            const allProductInterests = await ProductService.getProductInterestByUser(StorageHelper.getUserId())
+            const allProductInterests = await ProductService.getProductInterestByUser(
+                StorageHelper.getUserId()
+            )
 
-            res.data.forEach(element => {
-                if (allProductInterests.data.find(product => product.productId === element.id)){
-                    element.favourite = true;
-                }
-                else {
-                    element.favourite = false;
+            res.data.forEach((element) => {
+                if (allProductInterests.data.find((product) => product.productId === element.id)) {
+                    element.favourite = true
+                } else {
+                    element.favourite = false
                 }
             })
-    
+
             this.setState({ mainData: res.data })
             this.setState({ data: res.data })
         } else {
             this.setState({ mainData: res.data })
             this.setState({ data: res.data })
         }
-       
-
     }
 
     render() {
         return (
-            <Box sx={{padding: "2vw 0"}} className="container">
+            <Box sx={{ padding: "2vw 0" }} className="container">
                 <Carousel responsive={responsive}>
                     {this.state.recommendData.map((data, i) => (
                         <CarouselComponent
@@ -87,90 +86,111 @@ class Product extends Component {
                     ))}
                 </Carousel>
                 <br></br>
-                <Box style={{paddingTop: '1%'}}>
-                <input style={{height: 55, width: '60%'}}
-                    type="search"
-                    id="search"
-                    placeholder="Search for..."
-                    value={this.state.searchText}
-                    onChange={(e) => {
-                        if(this.state.filter !== ""){
-                            const dataSet = []
-                            this.state.mainData.forEach(element => {    
-                                if(element.category.toLowerCase().indexOf(this.state.filter.toLowerCase())  > -1) {
-                                    dataSet.push(element)
-                                }
-                            })
-
-                            const newDataSet = []
-                            this.setState({searchText: e.target.value})
-                            dataSet.forEach(element => {
-                                if(element.productName.toLowerCase().indexOf(e.target.value.toLowerCase())  > -1) {
-                                    newDataSet.push(element)
-                                }
-                            })
-                            this.setState({ data: newDataSet })
-                        }
-                        else {
-                            const dataSet = []
-                            this.setState({searchText: e.target.value})
-                            this.state.mainData.forEach(element => {
-                                if(element.productName.toLowerCase().indexOf(e.target.value.toLowerCase())  > -1) {
-                                    dataSet.push(element)
-                                }
-                            })
-                            this.setState({ data: dataSet })
-                        }
-                        
-                    }}
-                />
-                <FormControl style={{ width: 200 , left: '1%'}}>
-                    <InputLabel>Category</InputLabel>
-                    <Select
-                        label="Category"
-                        value = {this.state.filter}
+                <Box style={{ paddingTop: "1%" }}>
+                    <input
+                        style={{ height: 55, width: "60%" }}
+                        type="search"
+                        id="search"
+                        placeholder="Search for..."
+                        value={this.state.searchText}
                         onChange={(e) => {
-                            if (this.state.searchText !== ""){
+                            if (this.state.filter !== "") {
                                 const dataSet = []
-                                this.state.mainData.forEach(element => {
-                                    if(element.productName.toLowerCase().indexOf(this.state.searchText.toLowerCase())  > -1) {
+                                this.state.mainData.forEach((element) => {
+                                    if (
+                                        element.category
+                                            .toLowerCase()
+                                            .indexOf(this.state.filter.toLowerCase()) > -1
+                                    ) {
                                         dataSet.push(element)
                                     }
                                 })
-                                
+
                                 const newDataSet = []
-                                this.setState({ filter: e.target.value})
-                                dataSet.forEach(element => {    
-                                    if(element.category.toLowerCase().indexOf(e.target.value.toLowerCase())  > -1) {
+                                this.setState({ searchText: e.target.value })
+                                dataSet.forEach((element) => {
+                                    if (
+                                        element.productName
+                                            .toLowerCase()
+                                            .indexOf(e.target.value.toLowerCase()) > -1
+                                    ) {
                                         newDataSet.push(element)
                                     }
                                 })
                                 this.setState({ data: newDataSet })
-
                             } else {
                                 const dataSet = []
-                                this.setState({ filter: e.target.value})
-                                
-                                this.state.mainData.forEach(element => {    
-                                    if(element.category.toLowerCase().indexOf(e.target.value.toLowerCase())  > -1) {
+                                this.setState({ searchText: e.target.value })
+                                this.state.mainData.forEach((element) => {
+                                    if (
+                                        element.productName
+                                            .toLowerCase()
+                                            .indexOf(e.target.value.toLowerCase()) > -1
+                                    ) {
                                         dataSet.push(element)
                                     }
                                 })
                                 this.setState({ data: dataSet })
                             }
                         }}
-                    >
-                        <MenuItem value={""}>All</MenuItem>
-                        <MenuItem value={"BOOKS"}>Books</MenuItem>
-                        <MenuItem value={"ELECTRONICS"}>Electronics</MenuItem>
-                        <MenuItem value={"FASHION"}>Fashion</MenuItem>
-                        <MenuItem value={"FOOD"}>Food</MenuItem>
-                        <MenuItem value={"TOYS"}>Toys</MenuItem>
-                        <MenuItem value={"UTILITY"}>Utility</MenuItem>
-                        <MenuItem value={"VIDEO GAMES"}>Video Games</MenuItem>
-                        <MenuItem value={"OTHERS"}>Others</MenuItem>
-                    </Select>
-                </FormControl>
+                    />
+                    <FormControl style={{ width: 200, left: "1%" }}>
+                        <InputLabel>Category</InputLabel>
+                        <Select
+                            label="Category"
+                            value={this.state.filter}
+                            onChange={(e) => {
+                                if (this.state.searchText !== "") {
+                                    const dataSet = []
+                                    this.state.mainData.forEach((element) => {
+                                        if (
+                                            element.productName
+                                                .toLowerCase()
+                                                .indexOf(this.state.searchText.toLowerCase()) > -1
+                                        ) {
+                                            dataSet.push(element)
+                                        }
+                                    })
+
+                                    const newDataSet = []
+                                    this.setState({ filter: e.target.value })
+                                    dataSet.forEach((element) => {
+                                        if (
+                                            element.category
+                                                .toLowerCase()
+                                                .indexOf(e.target.value.toLowerCase()) > -1
+                                        ) {
+                                            newDataSet.push(element)
+                                        }
+                                    })
+                                    this.setState({ data: newDataSet })
+                                } else {
+                                    const dataSet = []
+                                    this.setState({ filter: e.target.value })
+
+                                    this.state.mainData.forEach((element) => {
+                                        if (
+                                            element.category
+                                                .toLowerCase()
+                                                .indexOf(e.target.value.toLowerCase()) > -1
+                                        ) {
+                                            dataSet.push(element)
+                                        }
+                                    })
+                                    this.setState({ data: dataSet })
+                                }
+                            }}>
+                            <MenuItem value={""}>All</MenuItem>
+                            <MenuItem value={"BOOKS"}>Books</MenuItem>
+                            <MenuItem value={"ELECTRONICS"}>Electronics</MenuItem>
+                            <MenuItem value={"FASHION"}>Fashion</MenuItem>
+                            <MenuItem value={"FOOD"}>Food</MenuItem>
+                            <MenuItem value={"TOYS"}>Toys</MenuItem>
+                            <MenuItem value={"UTILITY"}>Utility</MenuItem>
+                            <MenuItem value={"VIDEO GAMES"}>Video Games</MenuItem>
+                            <MenuItem value={"OTHERS"}>Others</MenuItem>
+                        </Select>
+                    </FormControl>
                 </Box>
                 <br></br>
                 <Box>
@@ -187,10 +207,17 @@ class Product extends Component {
                                     buttonLink={this.state.url + data.id}
                                     dateTime={data.dateTime}
                                     ownerName={data.ownerName}
-                                    favourite={data.favourite ?  data.favourite : false}
-                                    isOwner={ (StorageHelper.getUser()) ? ((StorageHelper.getUsername() === data.ownerName) ? true : false) : false }
-                                    userExist={ (StorageHelper.getUser()) ?  true : false} >
-                                    </CardComponent>
+                                    favourite={data.favourite ? data.favourite : false}
+                                    isOwner={
+                                        StorageHelper.getUser()
+                                            ? StorageHelper.getUsername() === data.ownerName
+                                                ? true
+                                                : false
+                                            : false
+                                    }
+                                    userExist={
+                                        StorageHelper.getUser() ? true : false
+                                    }></CardComponent>
                             </Grid>
                         ))}
                     </Grid>
